184ae76f624a50670272d2b01aa4963208c3bf63
Gooordeina <141111797+Gooordeina@users.noreply.github.com>
2023-08-08 14:55:53 +1200

    Testing upload

--- MainMenu.cs
+++ MainMenu.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour{
     // Start is called before the first frame update
+    // New comment
    public void PlayGame ()
    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
