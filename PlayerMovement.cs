2a88944c715e48879303efa356ca8a2e1ddc34f3
Gooordeina <141111797+Gooordeina@users.noreply.github.com>
2023-08-14 14:26:03 +1200

    Gravity
    To make player fall.

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -7,7 +7,9 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 8f;
+    public float gravity = -9.81f;
     
+    Vector3 velocity;
     void Update()
     {
         float x = Input.GetAxis("Horizontal");
@@ -16,5 +18,9 @@ public class PlayerMovement : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
