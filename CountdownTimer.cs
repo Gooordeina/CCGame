632eb21a7cefed228ca92667a61fa5406dffe5c0
Gooordeina <141111797+Gooordeina@users.noreply.github.com>
2023-09-04 19:13:15 +1200

    show the lose panel when the countdown is finished

--- CountdownTimer.cs
+++ CountdownTimer.cs
@@ -5,8 +5,10 @@ using UnityEngine.UI;
 using TMPro;
 public class CountdownTimer : MonoBehaviour
 {
-   public float currentTime = 0f;
-   public float startingTime = 60f;
+    public float currentTime = 0f;
+    public float startingTime = 60f;
+
+    public GameObject losepanel;
 
    [SerializeField] TextMeshProUGUI CountdownText;
     void Start()
@@ -22,6 +24,14 @@ public class CountdownTimer : MonoBehaviour
         if (currentTime <=0)
         {
             currentTime=0;
+        }
+
+           if (currentTime <= 0)
+        {
+            losepanel.SetActive(true);
+            Time.timeScale = 0f;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
         }
     }
 }
