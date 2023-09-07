a00f3106bee238682092626bfe5d59e9a6320b07
Gooordeina <141111797+Gooordeina@users.noreply.github.com>
2023-09-07 16:17:13 +1200

    2nd Version of upanddown animation of the object
    Instead of using two complicated while loops, I have came up with an idea of using a "for" loop to make the object following the pattern of a sin function

--- Float.cs
+++ Float.cs
@@ -1,50 +1,28 @@
 using System.Collections;
 using UnityEngine;
 
+
 public class Float : MonoBehaviour
 {
-    public float moveDistance = 1.0f; // The distance to move up and down.
-    public float moveSpeed = 1.0f;    // The speed of the animation.
-
-    private Vector3 startPosition;
-
-    private void Start()
+    int i = 1;
+    public float offsetamount;
+    public float amount;
+    public float x = 0;
+    void Update()
     {
-        startPosition = transform.position;
-        StartCoroutine(AnimateUpDown());
-    }
+        x = Time.time;  
 
-    private IEnumerator AnimateUpDown()
-    {
-        while (true)
+        offsetamount = Mathf.Sin(x) * amount;
+        foreach(GameObject cube in GameObject.FindGameObjectsWithTag("PickUpObject"))
         {
-            Vector3 targetPosition = startPosition + Vector3.up * moveDistance;
-            float journeyLength = Vector3.Distance(transform.position, targetPosition);
-            float startTime = Time.time;
 
-            while (Time.time - startTime < journeyLength / moveSpeed)
-            {
-                float distanceCovered = (Time.time - startTime) * moveSpeed;
-                float fractionOfJourney = distanceCovered / journeyLength;
-                transform.position = Vector3.Lerp(startPosition, targetPosition, fractionOfJourney);
-                yield return null;
-            }
-
-            yield return new WaitForSeconds(1.0f); // Wait at the top position for 1 second.
-
-            targetPosition = startPosition;
-            journeyLength = Vector3.Distance(transform.position, targetPosition);
-            startTime = Time.time;
-
-            while (Time.time - startTime < journeyLength / moveSpeed)
-            {
-                float distanceCovered = (Time.time - startTime) * moveSpeed;
-                float fractionOfJourney = distanceCovered / journeyLength;
-                transform.position = Vector3.Lerp(startPosition, targetPosition, fractionOfJourney);
-                yield return null;
-            }
-
-            yield return new WaitForSeconds(1.0f); // Wait at the bottom position for 1 second.
+            cube.transform.position += new Vector3(+0, +offsetamount, +0);
         }
+           
+
+
     }
+
+    
+    
 }
