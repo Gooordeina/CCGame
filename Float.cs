bcb3aab852f34d72313640e15c6a501f758d34c0
Gooordeina <141111797+Gooordeina@users.noreply.github.com>
2023-09-13 21:21:50 +1200

    Aim object animation stops when game is paused

--- Float.cs
+++ Float.cs
@@ -4,25 +4,27 @@ using UnityEngine;
 
 public class Float : MonoBehaviour
 {
+    //creating assets and variables
     int i = 1;
     public float offsetamount;
+    public GameObject pausescrpt;
     public float amount;
     public float x = 0;
     void Update()
     {
-        x = Time.time;  
-
+    //getting the gamepause variable from the PauseMenu Script
+        PauseMenu pm = pausescrpt.GetComponent<PauseMenu>();
+        x = Time.time;      
+    //calculate offest using time and sine wave
         offsetamount = Mathf.Sin(x) * amount;
-        foreach(GameObject cube in GameObject.FindGameObjectsWithTag("PickUpObject"))
+    //apply the offset to each cube if the game is not paused
+        if(pm.GameIsPaused == false)
         {
-
-            cube.transform.position += new Vector3(+0, +offsetamount, +0);
+            foreach(GameObject cube in GameObject.FindGameObjectsWithTag("PickUpObject"))
+            {
+                cube.transform.position += new Vector3(+0, +offsetamount, +0);
+            }   
         }
-           
-
-
+       
     }
-
-    
-    
 }
