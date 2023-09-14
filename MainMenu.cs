9920b964b31279bc6cdbe77e15aeace4c3daa7cd
Gooordeina <141111797+Gooordeina@users.noreply.github.com>
2023-09-14 22:13:03 +1200

    Comments

--- MainMenu.cs
+++ MainMenu.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class MainMenu : MonoBehaviour{
+public class MainMenu : MonoBehaviour
+{
     // Start is called before the first frame update
-    // New comment
+    // levelselection function. click the buton to bring the player to the level selectiion menu from the main menu
    public void PlayGame ()
    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
    }
+   //quit game function. quit the game after clicking
     public void QuitGame ()
     {
         Debug.Log("QUIT!");
