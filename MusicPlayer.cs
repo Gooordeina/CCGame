5c6021fd9451bb0a50822ff884e2f11394d97d2d
Gooordeina <141111797+Gooordeina@users.noreply.github.com>
2023-09-14 22:14:50 +1200

    Comments

--- MusicPlayer.cs
+++ MusicPlayer.cs
@@ -4,23 +4,29 @@ using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour
 {
+    //creating variables and assets
     public AudioSource Backgroundmusic; 
     public GameObject ObjectMusic;
 
+    //setting the initial volume at maximum which is 1.
     private float musicVolume = 1f;
     // Start is called before the first frame update
     void Start()
     {
+    //Find the game object with the tag "BGM" in the scene 
         ObjectMusic = GameObject.FindWithTag("BGM");
+    //Give the access of the Audio Source Component to Backgroundmusic
         Backgroundmusic = ObjectMusic.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+    //Enable me to control and update the background music's volume dynamically with the slide bar, ensuring that it aligns with user preferences and changes made during gameplay.
         Backgroundmusic.volume = musicVolume;
     }
 
+    //creating the updateVolume funcion so can be used in Unity
     public void updateVolume(float volume)
     {
         musicVolume = volume;
