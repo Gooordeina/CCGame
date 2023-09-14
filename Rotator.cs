a33edcc23341e82093314d337e1b49028de35777
Gooordeina <141111797+Gooordeina@users.noreply.github.com>
2023-09-14 22:16:24 +1200

    Comments

--- Rotator.cs
+++ Rotator.cs
@@ -8,6 +8,7 @@ public class Rotator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //rotate function. it rotates the object by (angles of 15, 30, 45 degrees), taking into account the frame time using "Time.deltaTime".
         transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
     }
 }
