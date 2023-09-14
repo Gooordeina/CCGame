48a369599fc2c510fc25fba9cccd3660c7baa498
Gooordeina <141111797+Gooordeina@users.noreply.github.com>
2023-09-14 22:10:02 +1200

    Comment

--- CountdownTimer.cs
+++ CountdownTimer.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 using TMPro;
 public class CountdownTimer : MonoBehaviour
 {
+    //setting up variables
     public float currentTime = 0f;
     public float startingTime = 60f;
 
@@ -12,20 +13,24 @@ public class CountdownTimer : MonoBehaviour
 
    [SerializeField] TextMeshProUGUI CountdownText;
     void Start()
-    {
+    {   
+        //at the start of the game, the current time equals to the starting time
         currentTime = startingTime;
     }
 
     void Update()
-    {
+    {   
+        //every second after the game starts, current time display decreeases by one
         currentTime -= 1 * Time.deltaTime;
         CountdownText.text = currentTime.ToString("0");
 
+        //ensure the current time does not go smaller than zero
         if (currentTime <=0)
         {
             currentTime=0;
         }
 
+        //lose function. if player didn't collect all the objects before the countdown finishes, lose panel pops out, game pauses, mouse cursor appears
            if (currentTime <= 0)
         {
             losepanel.SetActive(true);
